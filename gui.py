fedb2e469f3858a8bfe89b53f3cea3031c0ac08a
Anidh Singh <anidh.singh@gmail.com>
2018-07-21 19:09:45 +0530

    Added Msg When Clicking The Button

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 #Designing a simple GUI Tinkter App
 #Importing the library first
 from tkinter import *
+def printFull():
+    print("The Full Name Is:")
 root=Tk()
 #Using The Grid Layout Here!
 labelFirst=Label(root, text="Enter First Name")
@@ -12,6 +14,6 @@ labelLast.grid(row=1, column=0)
 entryFirst.grid(row=0, column=1)
 entryLast.grid(row=1, column=1)
 #Adding Buttons
-buttonOk=Button(root,text="OK")
+buttonOk=Button(root,text="OK",command=printFull)
 buttonOk.grid(row=2,column=0)
 root.mainloop()
