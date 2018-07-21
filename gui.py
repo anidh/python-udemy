495e98637a7a68ae1caaf5c1eee8f8cfe1243485
Anidh Singh <anidh.singh@gmail.com>
2018-07-21 19:32:25 +0530

    Added Classes Demo

--- gui.py
+++ gui.py
@@ -1,19 +1,19 @@
-#Designing a simple GUI Tinkter App
-#Importing the library first
 from tkinter import *
-def printFull():
-    print("The Full Name Is:")
+
+#using Classes Concept Here
+class tkinterMenu():
+    def __init__(self,newRoot):
+        self.frame=Frame(newRoot)
+        self.frame.pack
+
+        self.printButton=Button(newRoot,text="This will Print Something on Console",command=self.printSomething)
+        self.printButton.pack()
+
+        self.quitButton=Button(newRoot,text="X",command=self.frame.quit)
+        self.quitButton.pack()
+
+    def printSomething(self):
+        print("Something")
 root=Tk()
-#Using The Grid Layout Here!
-labelFirst=Label(root, text="Enter First Name")
-labelLast=Label(root, text="Enter Last Name")
-entryFirst=Entry(root)
-entryLast=Entry(root)
-labelFirst.grid(row=0, column=0)
-labelLast.grid(row=1, column=0)
-entryFirst.grid(row=0, column=1)
-entryLast.grid(row=1, column=1)
-#Adding Buttons
-buttonOk=Button(root,text="OK",command=printFull)
-buttonOk.grid(row=2,column=0)
+b=tkinterMenu(root)
 root.mainloop()
