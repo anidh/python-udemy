907f22949a6c90d83dd5dd354831106b4dc98b83
Anidh Singh <anidh.singh@gmail.com>
2018-07-21 16:58:20 +0530

    Fooled Around a bit!

--- gui.py
+++ gui.py
@@ -2,7 +2,18 @@
 #Importing the library first
 from tkinter import *
 root=Tk()
-whatToPrint=input("Ente The Text You Want To Print")
-label=Label(root,text=whatToPrint)
-label.pack()
-label.mainloop()
+# whatToPrint=input("Ente The Text You Want To Print")
+# label=Label(root,text=whatToPrint)
+# label.pack()
+##Adding Frames Here
+upperFrame=Frame(root)
+upperFrame.pack(side=TOP)
+lowerFrame=Frame(root)
+lowerFrame.pack(side=BOTTOM)
+##Adding the Buttons Here
+redButton=Button(upperFrame,text="Click Here",fg="Red")
+blueButton=Button(lowerFrame,text="Click Here",fg="Blue")
+#Packing the buttons
+redButton.pack()
+blueButton.pack()
+root.mainloop()
